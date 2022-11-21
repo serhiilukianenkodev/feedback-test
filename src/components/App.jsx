447ac5e34b36cee885d62dc6206334d79a7ae53c
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-11-21 10:48:34 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import { FeedbackControls } from 'components/FeedbackControls/FeedbackControls';
+import { Statistics } from 'components/Statistics/Statistics';
+
+import { Section } from 'components/Section/Section';
 
 export class App extends Component {
   state = {
@@ -17,13 +20,43 @@ export class App extends Component {
     });
   };
 
+  countTotalFeedback = () => {
+    const values = Object.values(this.state);
+    const total = values.reduce((count, value) => count + value, 0);
+
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const percentage = +((this.state.good * 100) / total).toFixed();
+
+    return total ? percentage : null;
+  };
+
   render() {
     return (
       <>
-        <FeedbackControls
-          options={Object.keys(this.state)}
-          handler={this.handleFeedback}
-        ></FeedbackControls>
+        <Section title="Please leave feedback">
+          <FeedbackControls
+            options={Object.keys(this.state)}
+            handler={this.handleFeedback}
+          ></FeedbackControls>
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() !== 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <p>There is no feedback yet</p>
+          )}
+        </Section>
       </>
     );
   }
